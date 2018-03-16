87dd4d47fcde654f7ff1cf9acf0c170a0a48a0d7
nandotur <37302109+nandotur@users.noreply.github.com>
2018-03-16 17:42:51 +0100

    Add files via upload

--- todo.sh
+++ todo.sh
@@ -12,18 +12,25 @@ contatore=$(echo $RIGA | cut -f1 -d" ")
 echo $contatore
 }
 
+cancellariga () {
+sed -i "/^$1 /d" $RAWLIST
+}
+
 if [ ! -f $RAWLIST ]; then
    echo "0"> $RAWLIST 
 fi
 [ $# -eq 0 ] && { todolist; exit 1; }
 
-echo "parametro 1=[$1]"
+#echo "parametro 1=[$1]"
 case "$1" in
 
 	add) 	let contatore=$(leggicontatore)+1
 		echo "$contatore $2 $3">>$RAWLIST
+		todolist
         ;;
-	del)	sed -i "/^$2 /d" $RAWLIST
+	del)	#sed -i "/^$2 /d" $RAWLIST
+		cancellariga $2
+		todolist
 	;;
 	*)	echo "comando non previsto, usage..."
 	;;
