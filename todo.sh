d3fef871a782c5549e9052b6cfb45a5b4b1b8733
nandotur <37302109+nandotur@users.noreply.github.com>
2018-03-23 15:07:02 +0100

    Add files via upload
    aggiunti commenti sulla validazione della data;
    eliminate istruzioni osolete.

--- todo.sh
+++ todo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 RAWLIST=./todolist.txt
-let RISEGREP=0
 let dataOK=0
 
 todolist () {
@@ -30,20 +29,24 @@ echo
 
 validadata () {
 #echo "dataDaValidare=[$1]"
+
+# estrae la data e inverte il formato da mm/dd/yyyy a dd/mm/yyyy
 GIORNOMDY="${1:3:1}${1:4:1}/${1:0:1}${1:1:1}/${1:6:1}${1:7:1}${1:8:1}${1:9:1}"
+
+# estrae l'orario in formato HH:MM
 ORA="${1:11:5}"
+
 #echo "ORA=$ORA"
 #echo "GIORNOMDY=$GIORNOMDY"
+
+# verifica la correttezza simultanea delle 3 condizioni su sintassi,giorno,ora
 echo $1 | egrep -q '^[0-3][0-9]/[0-1][0-9]/[0-9]{4}-[0-2][0-9]:[0-5][0-9]$' && (date -d $GIORNOMDY && date +"%H:%M" -d $ORA)>>/dev/null 2>&1
 
-#let RISEGREP=$?
-#echo "RISEGREP=[$RISEGREP]"
-#if (( $RISEGREP ))  
 if (( $? ))
 then let dataOK=0
 else let dataOK=1
 fi
-echo dataOK=[$dataOK]
+#echo dataOK=[$dataOK]
 }
 
 errore_data () {
@@ -62,8 +65,8 @@ fi
 case "$1" in
 
 	add) 	validadata $2
-		echo "add-Valido[$2]"
-		echo "dataOK=[$dataOK]"
+		#echo "add-Valido[$2]"
+		#echo "dataOK=[$dataOK]"
 		if  (( $dataOK ))
 		then	let contatore=$(leggicontatore)+1
 			echo "$contatore $2 $3">>$RAWLIST
@@ -72,8 +75,7 @@ case "$1" in
 			exit
 		fi
         ;;
-	del)	#sed -i "/^$2 /d" $RAWLIST
-		cancellariga $2
+	del)	cancellariga $2
 		todolist
 	;;
 	mod)	validadata $3
